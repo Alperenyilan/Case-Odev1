05cf6f1d3fee41db2bcff1ed4276a3a4ba485823
Alperen Yilan <alp_eren1616@hotmail.com>
2023-08-27 03:18:27 +0300

    son degisikller

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,5 @@
 import React from 'react';
-// import {Component} from 'react';
 import HeaderTop from './HeaderTop';
-// import LogninForm from './LogninForm';
-// import FlightList from './FlightList';
 import Bodycomponent from './Bodycomponent';
 import './FormS.css'
 
